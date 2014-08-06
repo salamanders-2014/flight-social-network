e4f1a27ad42e7dff721761487bd55f1b0923c520
quytx1102 <quytx1102@gmail.com>
2014-08-06 13:42:39 -0500

    udpated association

--- facebook-lite/app/models/post.rb
+++ facebook-lite/app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :poster, class_name: "User"
+  has_many :post_photos
   has_many :photos, through: :post_photos
   has_many :updates
   has_many :comments, through: :comment_posts
