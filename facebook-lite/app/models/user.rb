7a88f5e0b8a230f0fe85140bc2d54bbdc3bb1801
gond0026@umn.edu <gond0026@umn.edu>
2014-08-06 14:38:18 -0500

    added travis

--- facebook-lite/app/models/user.rb
+++ facebook-lite/app/models/user.rb
@@ -1,14 +1,12 @@
 class User < ActiveRecord::Base
   has_many :statuses
   has_many :comments, foreign_key: "commenter_id"
-  has_many :user_profile_pics
+
   has_many :profile_pics, through: :user_profile_pics, source: :photo
 
   # has many posts
   has_many :posts, foreign_key: "poster_id"
 
   # has many photos
-
-  has_many :post_photos, through: :posts
-  has_many :photos, through: :post_photos, source: :photo  
+  has_many :photos, through: :posts, source: :photos
 end
