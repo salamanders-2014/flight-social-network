625607fa7d5902e4b989b94313d055bfb6ac06fb
jermspeaks <jermspeaks@gmail.com>
2014-08-06 13:40:41 -0500

    comment spec started

--- facebook-lite/spec/models/comment_spec.rb
+++ facebook-lite/spec/models/comment_spec.rb
@@ -1,1 +1,17 @@
-comment_spec.rb
+require 'rails_helper'
+
+RSpec.describe Comment do
+	
+	before do
+		@user = User.new(first_name: "John", last_name: "Doe", email: "john@example.com")
+		@post = Post.new()
+		@comment = Comment.new(text: "Best Birthday Party Eva", commenter: @user, post: @post)
+	end
+
+	describe '#whatever' do
+	end
+
+
+
+
+end
