cda09fc4024a3fd4b83ec58e78d63e63e2a339c0
quytx1102 <quytx1102@gmail.com>
2014-08-07 14:05:54 -0500

    user now has friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
-  
   has_many :statuses
   has_many :comments, foreign_key: "commenter_id"
   has_many :user_profile_pics
@@ -12,4 +11,6 @@ class User < ActiveRecord::Base
   has_many :posts, foreign_key: "poster_id"
   has_many :photos, through: :posts # has many photos (in all posts)
   has_many :uploaded_photos, class_name: "Photo", foreign_key: "uploader_id"
+  has_many :user_friends
+  has_many :friends, through: :user_friends, source: :friend
 end
