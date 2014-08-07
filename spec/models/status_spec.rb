29497a5829c2c24ffeb5631ce7bd5e8d08463450
jermspeaks <jermspeaks@gmail.com>
2014-08-07 13:44:49 -0500

    status tests pass

--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 
 describe Status do
-  let(:user){User.create}
-  let(:status){Status.create(user_id: user.id, text: "Happy Cake Day!")}
+
+  before do
+    @user = User.create(first_name: "John", last_name: "Doe", email: "me@example.com", password: "12345")
+    @status = Status.create(user: @user, text: "Happy Cake Day!")
+  end
+
   it "knows its user" do
-    expect(status.user).to eq user
+    expect(@status.user).to be_a User
   end
 
   it 'should be valid with text' do
@@ -17,7 +21,8 @@ describe Status do
   end
 
   it "should not be an empty string" do
-    expect(status.text).not_to eq("")
+    expect(@status).to be_valid
+    expect(@status.text).not_to eq("")
   end
 
 end
