5c67819ecf1eeea9b5ad08fceb33d4945dc02bb5
gond0026@umn.edu <gond0026@umn.edu>
2014-08-07 18:03:24 -0500

    nextcmmit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def new
+    @users = User.all
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
+    end
+
 end
