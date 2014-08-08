8b821c03d1df42915330bac5b66edbb9c340c626
Andrew Adams & Timothy Howard <pair+andrewjadams3.tim2819@devbootcamp.com>
2014-08-08 11:09:38 -0500

    Working on post specs

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  has_many :photos
+  has_many :comments
+  belongs_to :poster, class_name: 'User', foreign_key: :user_id
+
+  validates :poster, presence: true
 end
