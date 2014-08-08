7b48028ee295c3665ec86c132e5daa181006bd8a
Jonathan Young & Timothy Howard <pair+jyoung217.tim2819@devbootcamp.com>
2014-08-08 11:24:25 -0500

    photo spec start

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,6 @@
 class Photo < ActiveRecord::Base
+	has_and_belongs_to_many :posts
+	belongs_to :uploader, class_name: "User", foreign_key: :user_id
+	belongs_to :poster
+	validates :description, :url, presence: true
 end
